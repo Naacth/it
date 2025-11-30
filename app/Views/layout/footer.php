65179c230ae17db34f95387e062835ce973282b1
Muhammad Fariz Ade Satriyo <fariz4384@gmail.com>
2025-11-30 18:42:49 +0700

    Update footer copyright text

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,3 +1,3 @@
 <footer class="jumbotron jumbotron-fluid mt-5 mb-0">
-    <div class="container text-center">Copyright &copy; <?= Date('Y') ?> Rika Ngebug</div>
+    <div class="container text-center">Copyright &copy; <?= Date('Y') ?> Rika Ngebuggggggggggg</div>
 </footer>
